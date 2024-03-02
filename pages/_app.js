a2b2303a1f17fa0afd96b5eec8d4c27ffc09379d
[Sumit] <[sumitadhikari166@gmail.com]>
2024-03-02 18:32:17 +0530

    socket server added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { SocketProvider } from "@/context/socket";
+
+export default function App( { Component, pageProps } ) {
+  return (
+    <SocketProvider>
+      <Component {...pageProps} />
+    </SocketProvider>
+  );
 }
