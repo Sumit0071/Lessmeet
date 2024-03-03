a8a46e33b8458fef7657705926ea60aa679cd9a1
[Sumit] <[sumitadhikari166@gmail.com]>
2024-03-04 01:49:58 +0530

    stream setup

--- hooks/usePeer.js
+++ hooks/usePeer.js
@@ -1,21 +1,27 @@
+import { useSocket } from '@/context/socket';
+import { useRouter } from 'next/navigation';
 const { useState, useEffect, useRef } = require( 'react' );
 
 const usePeer = () => {
+    const socket = useSocket();
+    const roomId = useRouter().query.roomId;
     const [peer, setPeer] = useState( null );
     const [myId, setMyId] = useState( '' );
     const isPeerSet = useRef( false );
 
     //set peerjs connection
     useEffect( () => {
-        if ( isPeerSet.current ) return;
+        if ( isPeerSet.current || !roomId || !socket ) return;
         isPeerSet.current = true;
+        let myPeer;
         ( async function initPeer() {
-            const myPeer = new ( await import( 'peerjs' ) ).default();
+            myPeer = new ( await import( 'peerjs' ) ).default();
             setPeer( myPeer );
 
             myPeer.on( 'open', ( id ) => {
                 console.log( `Your peer ID is ${id}` );
                 setMyId( id );
+                socket?.emit( 'join-room', roomId, id );
             } )
         } )()
     }, [] );
--- pages/[roomId].js
+++ pages/[roomId].js
@@ -2,11 +2,21 @@
 import usePeer from "@/hooks/usePeer";
 import { useSocket } from "@/context/socket";
 import { useEffect } from "react";
+import useMediaStream from "@/hooks/useMediaStream";
 
+import Player from "@/component/Player";
 const Room = () => {
     const socket = useSocket();
     const { peer, myId } = usePeer();
+    const { stream } = useMediaStream();
+    return (
+        <div className="videoPlayer">
+            <Player url={stream} muted playing playerId={myId} />
+        </div>
+    )
 
-}
+};
+
+
 
 export default Room;
